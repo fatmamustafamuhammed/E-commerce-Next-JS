ec3456a8cfb2d7d5b6086448be25fb0689323d36
Fatma Mustafa <86692955+fatmamustafamuhammed@users.noreply.github.com>
2026-06-02 02:55:10 +0300

    Change title in metadata from 'My Store' to 'STORE'

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
-  title: "My Store",
+  title: "STORE",
   description: "Buy Products",
 };
 
